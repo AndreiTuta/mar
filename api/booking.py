1a04b1053b1a18dc577b8d0a3d892aa30f918553
andreituta <tuta.andrei96@gmail.com>
2021-07-30 12:28:26 +0300

    Change cors and add logging

--- api/booking.py
+++ api/booking.py
@@ -1,29 +1,37 @@
 from flask import Blueprint, request
 from api.models import *
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import cross_origin
 
-booking_blueprint=Blueprint("booking", __name__)
+booking_blueprint = Blueprint("booking", __name__)
+
 
 @booking_blueprint.route("/new", methods=["POST"])
+@cross_origin(origin='*')
 def new():
-    b = Booking(request.args.get('customer'),request.args.get('date'))
+    b = Booking(request.args.get('customer'), request.args.get('date'))
     b.add()
     return b.as_dict()
 
+
 @booking_blueprint.route("/<id>", methods=["GET"])
+@cross_origin(origin='*')
 def fetch_booking(id: str):
-    b= Booking.query.filter_by(id=id).first()
-    return b.as_dict()
+    b = Booking.query.filter_by(id=id).first()
+    if b is not None:
+        return b.as_dict()
+    return "", 404
+
 
 @booking_blueprint.route("/period", methods=["GET"])
-@cross_origin()
+@cross_origin(origin='*')
 def fetch_bookings_in_period():
     start = request.args.get('start')
     end = request.args.get('end')
     b = Booking.query.filter(Booking.date > start, Booking.date < end).all()
     results = {}
-    for booking in b:
-        results[booking.id] = booking.as_dict()
-    return results
-    
+    if len(b) > 0:
+        for booking in b:
+            results[booking.id] = booking.as_dict()
+        return results
+    return "", 404
--- api/models.py
+++ api/models.py
@@ -9,60 +9,62 @@ db = SQLAlchemy()
 class ModelClass():
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-    
+
     def add(self):
         try:
+            print(f"Adding {self}")
             db.session.add(self)
             db.session.commit()
         except IntegrityError as e:
             print(f"Error processing new add")
             raise ValueError(e)
-        
+
 
 class Customer(db.Model, ModelClass):
-    __tablename__="customer"
-    
-    id = db.Column(db.Integer, primary_key=True) 
+    __tablename__ = "customer"
+
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     guid = db.Column(db.String, nullable=False)
     # populated when fetched
     bookings = []
-    
-    
+
     def __init__(self, name, last_name, email):
         self.name = name
         self.last_name = last_name
         self.email = email
         self.guid = str(uuid.uuid4())
-        
+
     def as_dict(self):
-           dicti =  {c.name: getattr(self, c.name) for c in self.__table__.columns}
-           dicti["bookings"] = self.bookings
-           return dicti
-        
-    def update(self, name, last_name, email,plate):
+        dicti = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        dicti["bookings"] = self.bookings
+        return dicti
+
+    def update(self, name, last_name, email, plate):
         if name != '':
             print(f"Updating {self.id} name {self.name} to {name}")
             self.name = name
         if last_name != '':
-            print(f"Updating {self.id} last_name {self.last_name} to {last_name}")
+            print(
+                f"Updating {self.id} last_name {self.last_name} to {last_name}")
             self.last_name = last_name
         if email != '':
             print(f"Updating {self.id} email {self.email} to {email}")
             self.email = email
         db.session.flush()
         db.session.commit()
-        
+
+
 class Booking(db.Model, ModelClass):
-    __tablename__="booking"
-    
-    id = db.Column(db.Integer, primary_key=True) 
+    __tablename__ = "booking"
+
+    id = db.Column(db.Integer, primary_key=True)
     customer = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
-    
-    def __init__(self, customer:int, date:str):
+
+    def __init__(self, customer: int, date: str):
         self.customer = customer
         format = "%Y-%m-%dT%H:%M:%S.000Z"
         dt_object = datetime.datetime.strptime(date, format)
